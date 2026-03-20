67bbd465dfd995eb1e83fc57add3045661ce5b95
a21-null <a21andresdc1@gmail.com>
2026-03-20 11:32:37 +0100

    no me acuerdo que hice

---
+++ petcare-frontend/assets/js/main.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function name() {
+    document.querySelector('.menu-hamburguesa-moviles').addEventListener('click', function () {
+        document.querySelector('.main-navbar').classList.toggle('menu-abierto');
+    });
+})
