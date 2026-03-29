ebd1a8676aba1d648657afe2f35fe7e1b1b0ff1a
ma-r1 <redondelana@gmail.com>
2026-03-29 21:31:35 +0200

    datos falsos + urgencias con json

---
+++ petcare-frontend/assets/js/urgencias.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+  $.getJSON("/petcare-frontend/assets/json/clinicas.json", (datos) => {
+    let clinicas = datos.clinicas;
+    let contenedor = $(".contenedor-tarjetas");
+    let template = document.getElementsByTagName("template")[0];
+
+    clinicas.forEach(clinica =>{
+      let copia = template.content.cloneNode(true);
+      if(clinica.urgencias){
+        copia.querySelector('.foto-clinica').src=clinica.foto;
+        copia.querySelector('.titulo-clinica').textContent=clinica.nombre;
+        copia.querySelector('.texto-ubicacion').textContent=clinica.ubicacion;
+        copia.querySelector('.texto-horario').textContent=clinica.horario;
+        copia.querySelector('.texto-telefono').textContent=clinica.telefono;
+        copia.querySelector('.llamar-ahora').href="tel:"+clinica.telefono;
+        contenedor.append(copia);
+      }
+      
+    });
+  });
+});
